27196784c79f8cbd1b319946a11dc6c6a6d4b0d0
Gabriel Henrique <henriquegabriel07007@gmail.com>
2024-05-15 20:02:06 -0300

    commit first

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
     <header className={classes.header}>
       <div className={classes.container}>
         <Link to="/" className={classes.logo}>
-          EasyBite
+          EasyBites
         </Link>
         <nav>
           <ul>
